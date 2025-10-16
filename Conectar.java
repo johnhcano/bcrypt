4296384fc3a3910aeff61cffd3eec1ba71e5df0d
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-10-16 00:54:47 +0000

    archivos

---
+++ Conectar.java
@@ -0,0 +1,21 @@
+public class Conectar {
+
+    private MD5 md5;
+
+    public Conectar() {
+        md5 = new MD5();
+    }
+
+    public String bcrypt(Usuario usuario, int cost) {
+        if (cost >= 1) {
+            // System.out.println("cost:"+cost);
+            usuario.setContraseniaUsuario(salt(usuario));
+            bcrypt(usuario, cost - 1);
+        }
+        return usuario.getContraseniaUsuario();
+    }
+
+    public String salt(Usuario usuario) {
+        return md5.getMd5("" + usuario.getIdUsuario() + usuario.getContraseniaUsuario());
+    }
+}
